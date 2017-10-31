c09ecc734bb1dcf543835feb691b658fdf549ba0
Artemio Cabelin <artemiocabelin@gmail.com>
2017-10-30 19:52:16 -0700

    add chai assertions

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -11,8 +11,13 @@ function AuthController() {
     function isAuthorizedAsync(neededRole, cb) {
         setTimeout(function(){ cb(roles.indexOf(neededRole) >= 0)}, 0);
     }
+    function isAuthorizedPromise(neededRole, cb) {
+        return new Promise(function(resolve) {
+            setTimeout(function(){ resolve(roles.indexOf(neededRole) >= 0)}, 0);
+        });
+    }
 
-    return {isAuthorized, isAuthorizedAsync, setRoles};
+    return {isAuthorized, isAuthorizedAsync, setRoles, isAuthorizedPromise};
 }
 
 module.exports = AuthController();
--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -1,5 +1,11 @@
 var assert = require('assert');
 var authController = require('../../controllers/auth.controller');
+var expect = require('chai').expect;
+var should = require('chai').should();
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+chai.should();
 
 // ---This beforeEach() will attach to the global describe and will run before any test runs
 // beforeEach(function () {
@@ -21,19 +27,24 @@ describe('AuthController', function() {
     // beforeEach('this function is erroring intentionally',function erroringFunction() {
     //     throw({error: 'error'});
     // })
+
     describe('isAuthorized', function() {
+        
         it('Should return false if not authorized', function() {
+            var isAuth = authController.isAuthorized('admin');
             // this.skip if you want mocha to skip this 'it' test
-            assert.equal(false, authController.isAuthorized('admin'));
+            expect(isAuth).to.be.false;
         })
         it('Should return true if authorized', function() {
             authController.setRoles(['user','admin']);
-            assert.equal(true, authController.isAuthorized('admin'));
+            var isAuth = authController.isAuthorized('admin');
+            isAuth.should.be.true;
         })
         it('should not allow get if not authorized')
         it('should allow get if authorized')
     })
-    describe.only('isAuthorizedAsync', function() {
+
+    describe('isAuthorizedAsync', function() {
         it('Should return false if not authorized', function(done) {
             this.timeout(2500);
             authController.isAuthorizedAsync('admin', 
@@ -41,6 +52,12 @@ describe('AuthController', function() {
                 assert.equal(false, isAuth);
                 done();
             });
+        })
+    })
+
+    describe('isAuthorizedPromise', function() {
+        it('Should return false if not authorized', function() {
+           return authController.isAuthorizedPromise('admin').should.eventually.be.false;
         })
     })
 });
